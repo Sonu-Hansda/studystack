aa200cbd368ad25e4628b7e7f29ee5866b20c25e
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-03-30 16:29:56 +0530

    updated for modularity

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -60,7 +60,7 @@ class ProfileScreen extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor.withOpacity(0.1),
+        color: Theme.of(context).primaryColor.withAlpha(30),
         borderRadius: BorderRadius.circular(16),
       ),
       child: Row(
